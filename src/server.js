7f7b440dafb6e319b22320f0efe5a8af94cac4f1
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-03-23 09:44:07 +0700

    use .env file

--- src/server.js
+++ src/server.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const path = require('path')
+// Config dotenv để chạy file .env
+require('dotenv').config()
+
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3000; // nếu chưa khai báo port thì lấy port = 3000
+const hostname = process.env.HOST_NAME
+
+
+// config template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // khai báo route 
 app.get('/', (req, res) => {
     res.send('Hello World!') // trả về 1 chuỗi spring
 })
 
-
 app.get('/test', (req, res) => {
     res.render('sample');
 })
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
